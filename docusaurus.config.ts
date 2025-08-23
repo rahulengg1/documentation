104c9483d6d7fa250ee3be3f877abfaab9f4aa60
Rahul Sharma <rahulengg1@gmail.com>
2025-08-23 17:00:33 +0200

    Changes

---
+++ docusaurus.config.ts
@@ -0,0 +1,247 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import type * as Plugin from "@docusaurus/types/src/plugin";
+import type * as OpenApiPlugin from "docusaurus-plugin-openapi-docs";
+
+const config: Config = {
+  title: "My Site",
+  tagline: "Dinosaurs are cool",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+            routeBasePath: '/',
+            sidebarPath: require.resolve("./sidebars.ts"),
+            // Please change this to your repo.
+            // Remove this to remove the "edit this page" links.
+            editUrl:
+                "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            docItemComponent: "@theme/ApiItem", // Derived from docusaurus-theme-openapi
+        },
+        theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig:
+    {
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
+      navbar: {
+        title: "",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            to: "/",
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Guide",
+          },
+         {
+              type: "docSidebar",
+              sidebarId: "openApiSidebar", // matches sidebars.ts
+              position: "left",
+              label: "API",
+            },
+          {
+            type: 'docsVersionDropdown',
+            versions: [ '1.0', '2.0'],
+          },
+
+        ],
+      },
+      footer: {
+        style: "dark",
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        additionalLanguages: [
+          "ruby",
+          "csharp",
+          "php",
+          "java",
+          "powershell",
+          "json",
+          "bash",
+          "dart",
+          "objectivec",
+          "r",
+        ],
+      },
+      languageTabs: [
+        {
+          highlight: "python",
+          language: "python",
+          logoClass: "python",
+        },
+        {
+          highlight: "bash",
+          language: "curl",
+          logoClass: "curl",
+        },
+        {
+          highlight: "csharp",
+          language: "csharp",
+          logoClass: "csharp",
+        },
+        {
+          highlight: "go",
+          language: "go",
+          logoClass: "go",
+        },
+        {
+          highlight: "javascript",
+          language: "nodejs",
+          logoClass: "nodejs",
+        },
+        {
+          highlight: "ruby",
+          language: "ruby",
+          logoClass: "ruby",
+        },
+        {
+          highlight: "php",
+          language: "php",
+          logoClass: "php",
+        },
+        {
+          highlight: "java",
+          language: "java",
+          logoClass: "java",
+          variant: "unirest",
+        },
+        {
+          highlight: "powershell",
+          language: "powershell",
+          logoClass: "powershell",
+        },
+        {
+          highlight: "dart",
+          language: "dart",
+          logoClass: "dart",
+        },
+        {
+          highlight: "javascript",
+          language: "javascript",
+          logoClass: "javascript",
+        },
+        {
+          highlight: "c",
+          language: "c",
+          logoClass: "c",
+        },
+        {
+          highlight: "objective-c",
+          language: "objective-c",
+          logoClass: "objective-c",
+        },
+        {
+          highlight: "ocaml",
+          language: "ocaml",
+          logoClass: "ocaml",
+        },
+        {
+          highlight: "r",
+          language: "r",
+          logoClass: "r",
+        },
+        {
+          highlight: "swift",
+          language: "swift",
+          logoClass: "swift",
+        },
+        {
+          highlight: "kotlin",
+          language: "kotlin",
+          logoClass: "kotlin",
+        },
+        {
+          highlight: "rust",
+          language: "rust",
+          logoClass: "rust",
+        },
+      ],
+    } satisfies Preset.ThemeConfig,
+
+  plugins: [
+    [
+      "docusaurus-plugin-openapi-docs",
+      {
+        id: "openapi",
+        docsPluginId: "classic",
+        config: {
+          petstore: {
+            specPath: "examples/petstore.yaml",
+            outputDir: "docs/petstore",
+            version: "current",
+            label: "API 1.0",
+            baseUrl: "docs/api",
+            downloadUrl:
+              "https://raw.githubusercontent.com/PaloAltoNetworks/docusaurus-template-openapi-docs/main/examples/petstore.yaml",
+            sidebarOptions: {
+              groupPathsBy: "tag",
+              categoryLinkSource: "tag",
+            },
+            versions: {
+                    "1.0": {
+                      label: "API 1.0",
+                      baseUrl: "docs/api",
+                      specPath: "examples/petstore-1.0.yaml",
+                      outputDir: "versioned_docs/version-1.0/petstore",
+                    },
+                  },
+                } satisfies OpenApiPlugin.Options,
+            } satisfies Plugin.PluginOptions,
+        },
+   ]
+  ],
+  themes: ["docusaurus-theme-openapi-docs",
+      [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        docsRouteBasePath: '/',
+        // For Docs using Chinese, it is recomended to set:
+        // language: ["en", "zh"],
+
+        // Customize the keyboard shortcut to focus search bar (default is "mod+k"):
+        // searchBarShortcutKeymap: "s", // Use 'S' key
+        // searchBarShortcutKeymap: "ctrl+shift+f", // Use Ctrl+Shift+F
+
+        // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
+        // forceIgnoreNoIndex: true,
+      }),
+    ],
+],
+};
+
+export default async function createConfig() {
+  return config;
+}
